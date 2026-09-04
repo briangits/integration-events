4bea157f8b96af0456dbfd92a6ff2fe413f61f5b
briangits <briangits@proton.me>
2026-09-04 15:38:54 +0300

    feat(message-brokers): add consumer

--- message-brokers/core/consumer/build.gradle.kts
+++ message-brokers/core/consumer/build.gradle.kts
@@ -2,3 +2,14 @@ plugins {
     alias(kt.plugins.multiplatform)
     alias(kt.plugins.multiplatform.android)
 }
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.messageBrokers.core.common)
+
+            implementation(kotlinx.coroutines)
+            implementation(kotlinx.serialization)
+        }
+    }
+}
---
+++ message-brokers/core/consumer/src/commonMain/kotlin/io/github/briangits/events/integration/broker/consumer/Consumer.kt
@@ -0,0 +1,56 @@
+package io.github.briangits.events.integration.broker.consumer
+
+import io.github.briangits.events.integration.broker.Message
+import io.github.briangits.events.integration.broker.Route
+import io.github.briangits.events.integration.metadata.Metadata
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import kotlinx.serialization.BinaryFormat
+import kotlinx.serialization.serializer
+
+abstract class Consumer(val format: BinaryFormat) {
+    abstract suspend fun consume(route: Route): Flow<Message<ByteArray>>
+
+    suspend inline fun <reified T> consume(route: Route): Flow<Message<T>> =
+        consume(route).map {
+            val data = format.decodeFromByteArray(serializer<T>(), it.data)
+
+            Message(
+                route = it.route,
+                key = it.key,
+                metadata = it.metadata,
+                data = data
+            )
+        }
+}
+
+suspend inline fun <reified T> Consumer.consume(
+    topic: String,
+    name: String = T::class.simpleName!!
+): Flow<Message<T>> = consume<T>(Route(topic, name))
+
+suspend inline fun <reified T> Consumer.subscribe(
+    route: Route,
+    scope: CoroutineScope? = null,
+    crossinline block: suspend (data: T, metadata: Metadata) -> Unit
+): Job {
+    val subscriptionScope = scope ?: CoroutineScope(currentCoroutineContext())
+
+    return subscriptionScope.launch(start = CoroutineStart.UNDISPATCHED) {
+        consume<T>(route).collect {
+            block(it.data, it.metadata)
+        }
+    }
+}
+
+suspend inline fun <reified  T> Consumer.subscribe(
+    topic: String,
+    name: String = T::class.simpleName!!,
+    scope: CoroutineScope? = null,
+    crossinline block: suspend (data: T, metadata: Metadata) -> Unit
+): Job = subscribe<T>(route = Route(topic, name), scope, block)
