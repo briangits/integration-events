7a111f86fc61ab7b8ced7a2f5b6e85fe0ca57e5e
briangits <briangits@proton.me>
2026-09-05 10:08:32 +0300

    refactor(message-broker): strip publisher API
    - remove data serialization
    - remove publish & message construction helpers

--- message-broker/publisher/src/commonMain/kotlin/io/github/briangits/events/integration/broker/publisher/Publisher.kt
+++ message-broker/publisher/src/commonMain/kotlin/io/github/briangits/events/integration/broker/publisher/Publisher.kt
@@ -1,39 +1,7 @@
 package io.github.briangits.events.integration.broker.publisher
 
 import io.github.briangits.events.integration.broker.Message
-import io.github.briangits.events.integration.broker.Route
-import io.github.briangits.events.integration.metadata.Metadata
-import io.github.briangits.events.integration.serialization.Serializer
-import kotlinx.serialization.serializer
 
-abstract class Publisher(val serializer: Serializer) {
-    abstract suspend fun publish(message: Message<ByteArray>)
-
-    suspend inline fun <reified T> publish(message: Message<T>) =
-        publish(
-            message.let {
-                val value = serializer.serialize(it.data, serializer<T>())
-                Message(route = it.route, metadata = it.metadata, data = value)
-            }
-        )
-
-    suspend inline fun <reified T> publish(
-        data: T,
-        topic: String,
-        name: String = T::class.simpleName!!,
-        key: String? = null,
-        block: Metadata.() -> Unit = {}
-    ) {
-        val metadata = Metadata(serializer)
-            .apply { block() }
-
-        publish(
-            Message(
-                route = Route(topic, name),
-                key = key,
-                metadata = metadata,
-                data = data
-            )
-        )
-    }
+interface Publisher {
+    suspend fun publish(message: Message<ByteArray>)
 }
